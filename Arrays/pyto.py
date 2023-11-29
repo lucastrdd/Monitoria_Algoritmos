702c7fcc467fda4321994b06bffde9ab8b0ebaf0
Lucas Trindade <lucasmedeirostrindade@gmail.com>
2023-11-29 15:53:38 -0300

    deleção

--- Arrays/pyto.py
+++
@@ -1,7 +0,0 @@
-lista = []
-for i in range(0, 5):
-    dicionario = {"Título": str(input("Qual o nome do filme? ")), "Ano":
-                  str(input("Qual o ano do filme? "))}
-    lista.append(dicionario)
-
-print(lista)
